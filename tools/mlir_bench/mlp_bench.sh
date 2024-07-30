afa8ebe6b8a12ba90f95f34d5d4476445f39b809
Adam Siemieniuk <adam.siemieniuk@intel.com>
2024-07-30 21:26:28 +0200

    Align MLP sizes with TPP-MLIR benchmarks
    Resizes MLP benchmarks to align with existing MLIR tests.
    Tweaks model generator to expose full control over M,N,K
    dimensions of torch Linear layer.

--- tools/mlir_bench/mlp_bench.sh
+++ tools/mlir_bench/mlp_bench.sh
@@ -62,24 +62,24 @@ if [ "${BASELINE_MODEL}" ] && [ ${IS_DYNAMIC} ]; then
 fi
 
 # Kernel config.
-INPUT_SIZES=( 1024 2048 4096 8192 )
-OUTPUT_SIZES=( 128 256 512 )
+LAYERS=( 1024 2048 4096 8192 )
+MINI_BATCHES=( 128 256 512 )
 if [ ! "${DATA_TYPE}" ]; then
     DATA_TYPE="f32"
 fi
 MODEL_NAME="MLIR_MLP_BENCH.xml"
 
 echo "Result type: time [ms]"
-for OUT_SIZE in "${OUTPUT_SIZES[@]}"; do
-  echo "MLP - OUT: ${OUT_SIZE} INS: ${INPUT_SIZES[@]}"
-  for IN_SIZE in "${INPUT_SIZES[@]}"; do
+for MB in "${MINI_BATCHES[@]}"; do
+  echo "MLP - MB: ${MB} LAYERS: ${LAYERS[@]}"
+  for LAYER in "${LAYERS[@]}"; do
     # Generate model.
     if [ "${BASELINE_MODEL}" ]; then
         # Enable baseline model flag.
-        MODEL_CONFIG=(-b="${BASELINE_MODEL}[${OUT_SIZE},${OUT_SIZE},${IN_SIZE}]")
+        MODEL_CONFIG=(-b="${BASELINE_MODEL}[${MB},${LAYER},${LAYER}]")
     else
         # Generate default PyTorch MLP.
-        MODEL_CONFIG=(-l="linear[${IN_SIZE},${OUT_SIZE}] relu[]")
+        MODEL_CONFIG=(-l="linear[${MB},${LAYER},${LAYER}] relu[]")
     fi
     GEN_FLAGS=(-t ${DATA_TYPE} -n ${MODEL_NAME})
     if [ ${IS_DYNAMIC} ]; then
@@ -97,7 +97,7 @@ for OUT_SIZE in "${OUTPUT_SIZES[@]}"; do
         PRECISION="f16"
     fi
     if [ ${IS_DYNAMIC} ]; then
-        DATA_SHAPE=(-data_shape [${OUT_SIZE},${IN_SIZE}])
+        DATA_SHAPE=(-data_shape [${MB},${LAYER}])
     fi
     # Benchmark config. Disable parallelism.
     PERF_FLAGS="-niter 10000 -hint none -nstreams 1 -nthreads 1"
